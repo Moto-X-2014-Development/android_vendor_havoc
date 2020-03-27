6e4543d4a8d97764e5a65178a188aeb56a8a08de
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    [2/4] Port face unlock feature
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -82,6 +82,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Include AOSP audio files
 include vendor/lineage/config/aosp_audio.mk
 
